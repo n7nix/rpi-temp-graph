ac15a9f1b67a1eab9e0b38d28ffb3c9f4a76ce3c
n7nix <basil@pacabunga.com>
2020-08-10 12:46:42 -0700

    Use proper rrd file names.

--- db_rpitempbuilder.sh
+++ db_rpitempbuilder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# RRDCBW
+# RRD builder for RPi CPU temperature graph
 
 PATH=/usr/bin:/bin
 
-MNTPNT="/media/backup/rrd"
-RRDDIR="$MNTPNT/var/lib/cbw/rrdtemp"	# This should be the same as RRDDIR in db_cbwpupupdate.sh
+MNTPNT="$HOME"
+RRDDIR="$MNTPNT/var/lib/rpi/rrdtemp"	# This should be the same as RRDDIR in db_rpitempupupdate.sh
 
 #######################################################################################
 ### YOU SHOULD NOT HAVE TO EDIT ANYTHING BELOW THIS LINE ##############################
@@ -14,7 +14,7 @@ RRDDIR="$MNTPNT/var/lib/cbw/rrdtemp"	# This should be the same as RRDDIR in db_c
 clear
 
 echo ""
-echo "RRDcbw : database builder"
+echo "RRDrpi : database builder"
 echo "-----------------------------"
 echo "Are you sure you want to create the database files ?"
 echo -n "IT WILL OVERWRITE EXISTING DATA (y/n) "
@@ -35,10 +35,10 @@ then
 
 	if [ ! -d ${RRDDIR} ]
 	then
-		mkdir ${RRDDIR} -p
+		mkdir -p ${RRDDIR}
 	fi
 
-	rrdtool create ${RRDDIR}/highpumphouse.rrd \
+	rrdtool create ${RRDDIR}/rpicpu.rrd \
 	--start 1127253600 \
 	DS:highpump:GAUGE:600:-50:150 \
 	RRA:AVERAGE:0.5:1:600 \
@@ -54,25 +54,9 @@ then
         RRA:MAX:0.5:24:775 \
         RRA:MAX:0.5:288:797
 
-	rrdtool create ${RRDDIR}/lowpumphouse.rrd \
+	rrdtool create ${RRDDIR}/rpiamb.rrd \
         --start 1127253600 \
         DS:lowpump:GAUGE:600:-50:150 \
-        RRA:AVERAGE:0.5:1:600 \
-        RRA:AVERAGE:0.5:6:700 \
-        RRA:AVERAGE:0.5:24:775 \
-        RRA:AVERAGE:0.5:288:797 \
-        RRA:MIN:0.5:1:600 \
-        RRA:MIN:0.5:6:700 \
-        RRA:MIN:0.5:24:775 \
-        RRA:MIN:0.5:288:797 \
-        RRA:MAX:0.5:1:600 \
-        RRA:MAX:0.5:6:700 \
-        RRA:MAX:0.5:24:775 \
-        RRA:MAX:0.5:288:797
-
-	rrdtool create ${RRDDIR}/outsidepumphouse.rrd \
-        --start 1127253600 \
-        DS:outpump:GAUGE:600:-50:150 \
         RRA:AVERAGE:0.5:1:600 \
         RRA:AVERAGE:0.5:6:700 \
         RRA:AVERAGE:0.5:24:775 \
@@ -92,7 +76,7 @@ then
 	echo "Listing database files created under ${RRDDIR} :"
 	ls -lh ${RRDDIR}/
 	echo ""
-	echo "Now, you can setup the db_cbwpumpupdate.sh script and set up a cron to run it"
+	echo "Now, you can setup the db_rpitempupdate.sh script and a crontab entry to run it"
 
 else
 
