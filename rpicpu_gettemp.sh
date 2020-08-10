82bc388ec7778502e0e579d5d429cd64225d3a88
n7nix <basil@pacabunga.com>
2020-08-10 12:49:09 -0700

    Convert vcgencmd output to Fahrenheit.

--- rpicpu_gettemp.sh
+++ rpicpu_gettemp.sh
@@ -1,21 +1,21 @@
 #/bin/bash
 
-xmlbuf=$(curl -fsSk "http://10.0.42.31/state.xml" | grep "sensor")
-# xmlbuf=$(curl -fsSk "http://10.0.42.31/state.xml")
+temp=$(vcgencmd measure_temp | cut -f2 -d'=' | cut -f1 -d"'")
+temp_int=$(echo $temp | cut -f1 -d'.')
+temp_dec=$(echo $temp | cut -f2 -d'.')
 
-case $1 in
-    1)
-	echo "$(echo $xmlbuf | awk '{print $1}' | cut -d '>' -f2 | cut -d '<' -f1) "
-    	;;
-    2)
-	echo "$(echo $xmlbuf | awk '{print $2}' | cut -d '>' -f2 | cut -d '<' -f1) "
-    	;;
-    3)
-	echo "$(echo $xmlbuf | awk '{print $3}' | cut -d '>' -f2 | cut -d '<' -f1) "
-    	;;
-    *)
-	echo -n "$(echo $xmlbuf | awk '{print $1}' | cut -d '>' -f2 | cut -d '<' -f1) "
-	echo -n "$(echo $xmlbuf | awk '{print $2}' | cut -d '>' -f2 | cut -d '<' -f1) "
-	echo -n $xmlbuf | awk '{print $3}' | cut -d '>' -f2 | cut -d '<' -f1
-	;;
-esac
+if [ ! -z "$temp" ] ; then
+
+    echo "Compute Fahrenheit: $temp_int C"
+    if ((temp_dec >= 5)) ; then
+        ((temp_int++))
+        echo "Bump temp_int"
+    fi
+    temp_fah=$(( ((9 * temp_int)+160)/5 ))
+    temp_fah1=$(( ((9 * temp_int)/5)+32 ))
+
+else
+    echo "Error temp: $temp"
+fi
+
+echo "Temp raw: $temp, int: $temp_int, dec: $temp_dec, fah: $temp_fah, fah1: $temp_fah1"
