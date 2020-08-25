b8eef10815187dbd985f47ca185b74be0151080f
n7nix <basil@pacabunga.com>
2020-08-25 08:35:16 -0700

    Add start_service & update_graph_scripts functions.

--- tempgraph_install.sh
+++ tempgraph_install.sh
@@ -2,9 +2,13 @@
 
 scriptname="`basename $0`"
 user="pi"
+
+BINDIR="/home/$user/bin"
+
 REPO_NAME="rpi-temp-graph"
 CP="cp"
 CHOWN="sudo chown"
+SYSTEMCTL="sudo systemctl"
 
 # Refresh scripts only
 b_refresh_only=false
@@ -24,10 +28,41 @@ function is_pkg_installed() {
     return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed" >/dev/null 2>&1)
 }
 
+# ===== function start_service
+
+function start_service() {
+    service="$1"
+    echo "Starting: $service"
+
+    systemctl is-enabled "$service" > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        echo "ENABLING $service"
+        $SYSTEMCTL enable "$service"
+        if [ "$?" -ne 0 ] ; then
+            echo "Problem ENABLING $service"
+            exit
+        fi
+    fi
+
+    $SYSTEMCTL --no-pager start "$service"
+    if [ "$?" -ne 0 ] ; then
+        echo "Problem starting $service"
+        systemctl status $service
+        exit
+    fi
+}
+
 # ===== function cfg_lighttpd
+# Configure & start lighttpd daemon
+# Modifys or creates files:
+#   /var/log/lighttpd/error.log
+#   /etc/lighttpd/lighttpd.conf
+#   /etc/lighttpd/conf-available/15-fastcgi-php
+#   /etc/php/$PHPVER/fpm/php.ini
 
 function cfg_lighttpd() {
 
+    echo "start function: ${FUNCNAME[0]}"
     pkg_name="apache2"
     is_pkg_installed $pkg_name
     if [ $? -eq 0 ] ; then
@@ -41,6 +76,7 @@ function cfg_lighttpd() {
         sudo apt-get install -y -q $pkg_name
     fi
 
+    echo "DEBUG: setup lighttpd log file"
     if [ ! -d "/var/log/lighttpd" ] ; then
         sudo mkdir -p "/var/log/lighttpd"
         sudo touch "/var/log/lighttpd/error.log"
@@ -48,21 +84,51 @@ function cfg_lighttpd() {
 
     $CHOWN -R www-data:www-data "/var/log/lighttpd"
 
-    lighttpd-enable-mod fastcgi
-    lighttpd-enable-mod fastcgi-php
+    echo "DEBUG: About to enable lighttpd modules: CHOWN=$CHOWN"
+    sudo lighttpd-enable-mod fastcgi
     ls -l /etc/lighttpd/conf-enabled
 
+    grep -i "deny access to /data directory" $lighttpdcfg_file > /dev/null
+    retcode=$?
+    echo "DEBUG: grep retcode: $retcode"
+    if [ "$retcode" -ne 0 ] ; then
     # If you're using lighttpd, add the following to your configuration file:
-    sudo cat << 'EOT' >> $lighttpdcfg_file
+        sudo tee -a $lighttpdcfg_file > /dev/null << 'EOT'
 # deny access to /data directory
 $HTTP["url"] =~ "^/data/" {
      url.access-deny = ("")
 }
 EOT
+    fi
+
+    if [ ! -f /etc/lighttpd/cgi.conf ] ; then
+    sudo tee -a /etc/lighttpd/cgi.conf > /dev/null  << EOT
+server.modules += ( "mod_cgi" )
+
+cgi.assign                 = ( ".pl"  => "/usr/bin/perl",
+                               ".cgi" => "/usr/bin/perl",
+                               ".rb"  => "/usr/bin/ruby",
+                               ".erb" => "/usr/bin/eruby",
+                               ".py"  => "/usr/bin/python",
+                               ".php" => "/usr/bin/php-cgi" )
+
+index-file.names           += ( "index.pl",   "default.pl",
+                               "index.rb",   "default.rb",
+                               "index.erb",  "default.erb",
+                               "index.py",   "default.py",
+                               "index.php",  "default.php" )
+EOT
+    fi
+
+    if [ 1 -eq 0 ] ; then
+
+    echo "DEBUG: modify 15-fastcgi-php.conf"
+    sudo lighttpd-enable-mod fastcgi-php
+
     # back this file up until verified
     lighttpd_conf_avail_dir="/etc/lighttpd/conf-available"
     sudo cp $lighttpd_conf_avail_dir/15-fastcgi-php.conf $lighttpd_conf_avail_dir/15-fastcgi-php.bak1.conf
-    sudo cat << EOT > $lighttpd_conf_avail_dir/15-fastcgi-php.conf
+    sudo tee -a $lighttpd_conf_avail_dir/15-fastcgi-php.conf > /dev/null << EOT
 # -*- depends: fastcgi -*-
 # /usr/share/doc/lighttpd/fastcgi.txt.gz
 # http://redmine.lighttpd.net/projects/lighttpd/wiki/Docs:ConfigurationOptions#mod_fastcgi-fastcgi
@@ -85,6 +151,8 @@ EOT
         echo "   ERROR: php config file: $php_filename does not exist"
     fi
 
+    fi # end if 1 = 0
+
     # Change document root directory
     # server.document-root should be: /var/www
 #    sed -i -e '/server\.document-root / s/server\.document-root .*/server\.document-root = \"\/var\/www\/"/' /etc/lighttpd/lighttpd.conf
@@ -94,7 +162,71 @@ EOT
 
     # Restart lighttpd
     echo "lighttpd force-reload"
+    start_service lighttpd
+
     sudo service lighttpd force-reload
+}
+
+# ===== function update_graph_scripts
+function update_graph_scripts() {
+
+    CGI_BINDIR="/var/www/cgi-bin"
+    FILE_LIST="db_rpitempupdate.sh db_rpitempbuilder.sh rpicpu_gettemp.sh rpiamb_gettemp.sh dht11/dht11_temp rpitemp.cgi"
+    for file_name in `echo ${FILE_LIST}` ; do
+
+        # Look at filename extension
+        exten="${file_name##*.}"
+        if [ "$exten" = "cgi" ] ; then
+            diff $file_name $CGI_BINDIR/ > /dev/null 2>&1
+            retcode="$?"
+            destdir=$CGI_BINDIR
+            CP="sudo cp"
+            # Verify CGI_BINDIR exists
+            if [ ! -d "$CGI_BINDIR" ] ; then
+                sudo mkdir -p "$CGI_BINDIR"
+            fi
+        else
+            diff $file_name $BINDIR > /dev/null 2>&1
+            retcode="$?"
+            destdir=$BINDIR
+        fi
+
+        # Determine if executable exists to read dht11 sensor
+        if [[ -x "$file_name" ]] ; then
+            dbgecho "$file_name found executable"
+        else
+            pushd dht11
+            make
+            if [ "$?" -eq 0 ] ; then
+                echo "$file_name build successful"
+            else
+            echo "$file_name build FAILED"
+            fi
+            popd
+            $CHOWN -R $user:$user dht11
+        fi
+
+        case $retcode in
+            0)
+                echo "No update required for file: $file_name"
+            ;;
+            1)
+                echo "Updating file: $file_name to destination: $destdir"
+                $CP -u $file_name $destdir
+            ;;
+            2)
+                echo "Copy file: $file_name to destination: $destdir"
+                $CP $file_name $destdir
+
+                echo "DEBUG: Check directory: copy return: $?"
+                ls -al $destdir
+            ;;
+            *)
+                echo "Return code: $retcode, not recognized"
+            ;;
+        esac
+    done
+    $CHOWN -R www-data:www-data "/var/www/cgi-bin"
 }
 
 # ===== function setup_crontab
@@ -122,7 +254,7 @@ function setup_crontab() {
 
 function usage() {
    echo "Usage: $scriptname [-r][-d][-h]" >&2
-   echo "   -r   Refresh files only"
+   echo "   -u   Update script files only"
    echo "   -d   Set debug flag"
    echo "   -h   Display this message"
    echo
@@ -144,7 +276,7 @@ while [[ $# -gt 0 ]] ; do
    key="$1"
 
    case $key in
-      -r|--refresh)
+      -u|--update)
          b_refresh_only=true
          echo "Refresh scripts only"
          ;;
@@ -195,71 +327,26 @@ echo "DEBUG only: change directory to: $SRC_DIR"
 cd $SRC_DIR
 pwd
 
-if $b_refresh_only ; then
-    echo
-    echo "Configure lighttpd"
-    #cfg_lighttpd
-    exit 0
-fi
-
 # For reference: get filename extension
-#filename=$(basename "$fullfile")
+# filename=$(basename "$fullfile")
 # extension="${filename##*.}"
 # filename="${filename%.*}"
 
 echo
-echo "Update rrd scripts"
+echo "Update graph scripts, don't touch RRD database files"
 
-BINDIR="/home/$user/bin"
-CGI_BINDIR="/var/www/cgi-bin"
-FILE_LIST="db_rpitempupdate.sh db_rpitempbuilder.sh rpicpu_gettemp.sh rpiamb_gettemp.sh dht11/dht11_temp rpitemp.cgi"
-for file_name in `echo ${FILE_LIST}` ; do
+update_graph_scripts
+if $b_refresh_only ; then
+    echo
+    echo "Updating scripts only"
+    exit 0
+fi
 
-    # Look at filename extension
-    exten="${file_name##*.}"
-    if [ "$exten" = "cgi" ] ; then
-        diff $file_name $CGI_BINDIR/ > /dev/null 2>&1
-        retcode="$?"
-        destdir=$CGI_BINDIR
-        CP="sudo cp"
-    else
-        diff $file_name $BINDIR > /dev/null 2>&1
-        retcode="$?"
-        destdir=$BINDIR
-    fi
+ echo "DEBUG: Configure lighttpd"
+ cfg_lighttpd
 
-    # Determine if executable exists to read dht11 sensor
-    if [[ -x "$file_name" ]] ; then
-        dbgecho "$file_name found executable"
-    else
-        pushd dht11
-        make
-        if [ "$?" -eq 0 ] ; then
-            echo "$file_name build successful"
-        else
-            echo "$file_name build FAILED"
-        fi
-        popd
-        $CHOWN -R $user:$user dht11
-    fi
-
-    case $retcode in
-        0)
-            echo "No update required for file: $file_name"
-            ;;
-        1)
-            echo "Updating file: $file_name to destination: $destdir"
-            $CP -u $file_name $destdir
-            ;;
-        2)
-            echo "Copy file: $file_name to destination: $destdir"
-            $CP $file_name $destdir
-            ;;
-        *)
-            echo "Return code: $retcode, not recognized"
-            ;;
-    esac
-done
+echo
+echo "Verify database files & directories exist"
 
 if [ -d "/home/$user/var/lib/rpi/rrdtemp" ] && [ -d "/home/$user/var/tmp/rpitemp" ] ; then
     echo "About to over write database files:"
@@ -267,13 +354,14 @@ if [ -d "/home/$user/var/lib/rpi/rrdtemp" ] && [ -d "/home/$user/var/tmp/rpitemp
     ls -al /home/$user/var/lib/rpi/rrdtemp
     ls -al /home/$user/var/tmp/rpitemp
     echo
-   read -p "Over write graph files? (y or n): " -n 1 -r REPLY
+    read -p "Over write graph files? (y or n): " -n 1 -r REPLY
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
         echo "Graph files unchanged ..."
     else
         $BINDIR/db_rpitempbuilder.sh
         $CHOWN -R $user:$user /home/$user/var
+        $CHOWN -R www-data:www-data /home/$user/var/tmp/rpitemp
     fi
 
 else
@@ -281,6 +369,7 @@ else
     mkdir -p "/home/$user/var/tmp/rpitemp"
     $BINDIR/db_rpitempbuilder.sh
     $CHOWN -R $user:$user /home/$user/var
+    $CHOWN -R www-data:www-data /home/$user/var/tmp/rpitemp
 fi
 
 setup_crontab
