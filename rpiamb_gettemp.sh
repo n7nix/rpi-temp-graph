f43c0e4d6dfc7e9b4ba9ce9bc989694de7deb11b
n7nix <basil@pacabunga.com>
2020-08-27 09:49:55 -0700

    Make GPIO variable easily changeable, add documentation.

--- rpiamb_gettemp.sh
+++ rpiamb_gettemp.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
+# rpiamb_gettemp.sh
+#
+# Get value of ambient temperature in degrees Fahrenheit
+# Calls dht11_temp C program to read DHT11 temperature sensor
+#
+# Any arguments on command line will set DEBUG flag
 DEBUG=
-REMOTE=false
-user=$(whoami)
-# local ambient temperature read
-getdht11_temp="/home/$user/bin/dht11_temp"
 
+# Set WiringPi GPIO pin number to use
+#  Use:  `gpio readall` to translate between WiringPi & BCM gpio pin numbers
+#  Use:  `pinout` to get 40 pin header number
+#   WiringPi   BCM  PI 40 pin header  DRAWS 8 pin Aux
+#   --------   ---  ---------------   ---------------
+#      0        17        11              n/a
+#     21         5        29              3 (IO5)
+#     22         6        31              2 (IO6)
+
+WIRINGPI_GPIO=0
+
+# Can run this script remotely using ssh
+# Need to create RSA keys so not prompted for password
+REMOTE=false
+IPADDR="10.0.42.37"
+
+user=$(whoami)
 # Check if there are any args on command line
 if (( $# != 0 )) ; then
     DEBUG=1
 fi
 
+# Set command to use
 if [ $REMOTE = true ] ; then
     # remote ambient temperature read
-    getdht11_temp="ssh pi@10.0.42.37 $HOME/bin/dht11_temp"
+    getdht11_temp="ssh pi@${IPADDR} $HOME/bin/dht11_temp $WIRINGPI_GPIO"
+else
+    # local ambient temperature read
+    getdht11_temp="/home/$user/bin/dht11_temp $WIRINGPI_GPIO"
 fi
 
-# For raspberry pi with DHT11 temperature sensor
+# Read the temperature sensor
 temp=$($getdht11_temp)
-loopcnt=0
 
+# Loop until some value is read
+loopcnt=0
 while [ -z "$temp" ] && [ $loopcnt -le 6 ]; do
     sleep .5
     temp=$($getdht11_temp)
