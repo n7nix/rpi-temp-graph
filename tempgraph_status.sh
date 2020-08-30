650f44b7c4ec81c01e8922a9ab7aa59d0f7beeee
n7nix <basil@pacabunga.com>
2020-08-30 10:15:37 -0700

    Add display of sensor values, reduce output of lighttpd daemon status.

--- tempgraph_status.sh
+++ tempgraph_status.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
+#
+DEBUG=
 
 scriptname="`basename $0`"
+
+# Set debug flag if any args on command line
+if (( $# != 0 )) ; then
+    DEBUG=1
+fi
 
 echo
 echo "=== status start: $(date) ==="
 echo
-echo "=== lighttpd daemon status ==="
-systemctl --no-pager status lighttpd
+echo "=== CPU & Ambient sensor check ==="
+
+program_name="rpicpu_gettemp.sh"
+# type command will return 0 if program is installed
+type -P "$program_name" &>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "Script: $program_name not installed"
+else
+   echo "Temp $program_name: $($program_name)"
+fi
+
+program_name="rpiamb_gettemp.sh"
+# type command will return 0 if program is installed
+type -P "$program_name" &>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "Script: $program_name not installed"
+else
+   echo "Temp $program_name: $($program_name)"
+fi
+
+# Get lighttpd daemon status
+systemctl --no-pager status lighttpd > /dev/null 2>&1
+retcode="$?"
+if [ "$retcode" -eq 0 ] ; then
+    lighty_status="OK"
+else
+    lighty_status="FAILED: $retcode"
+fi
+
+echo
+echo "=== lighttpd daemon status $lighty_status ==="
+
+# if debug flag is set display all of lighttpd status
+if [ ! -z "$DEBUG" ] ; then
+    systemctl --no-pager status lighttpd
+fi
 
 echo
 echo "=== lighttphd enabled modules ==="
